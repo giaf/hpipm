0b4b13530a3895d8341af10ac849ab73e9ffc42e
giaf <gianfri2008@hotmail.it>
2017-07-03 10:20:55 +0200

    fixed fabs in tools.c

--- test_problems/s_tools.c
+++ test_problems/s_tools.c
@@ -409,10 +409,10 @@ float onenorm(int row, int col, float *ptrA)
 	temp = 0;
 	for(j=0; j<col; j++)
 		{
-		temp = abs(*(ptrA+j*row));
+		temp = fabs(*(ptrA+j*row));
 		for(i=1; i<row; i++)
 			{
-			temp += abs(*(ptrA+j*row+i));
+			temp += fabs(*(ptrA+j*row+i));
 			}
 		if(j==0) max = temp;
 		else if(max>temp) temp = max;
