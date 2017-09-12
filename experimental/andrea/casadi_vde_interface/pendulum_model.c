eaaedd1cb3e70bea30c7f0fee18a0fdb55484ae8
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2017-09-12 12:32:13 +0200

    adding casadi vde interface

---
+++ experimental/andrea/casadi_vde_interface/pendulum_model.c
@@ -0,0 +1,35 @@
+#include "pendulum_model.h"
+
+void VDE_fun_pendulum(const double* in, double* out,
+                      int (*vde)(const double**, double**, int*, double*,
+                                 int)) {
+
+    const int NX = 4;
+    const int NU = 1;
+    const double* x = in;
+    const double* Sx = in + NX;
+    const double* Su = in + NX + NX * NX;
+    const double* u = in + NX + NX * (NX + NU);
+
+    double* x_out = out;
+    double* Sx_out = out + NX;
+    double* Su_out = out + NX + NX * NX;
+
+    const double* casadi_arg[4];
+    double* casadi_res[3];
+
+    casadi_arg[0] = x;
+    casadi_arg[1] = Sx;
+    casadi_arg[2] = Su;
+    casadi_arg[3] = u;
+
+    casadi_res[0] = x_out;
+    casadi_res[1] = Sx_out;
+    casadi_res[2] = Su_out;
+
+    int* iw = 0;
+    double* w = 0;
+    int mem = 0;
+
+    vde(casadi_arg, casadi_res, iw, w, mem);
+}
