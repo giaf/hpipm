c74e8967a63cdae28201731fdfd1b8c7506f5ee7
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-06-25 18:01:27 +0200

    added __init__ in subfolders, simplified namings

--- interfaces/python/hpipm_python/hpipm_python/__init__.py
+++ interfaces/python/hpipm_python/hpipm_python/__init__.py
@@ -4,8 +4,6 @@ This is the Python interface to the QP solver HPIPM.
 Copyright 2018 (c) Andrea Zanelli, Gianluca Frison 
 """
 
-from . import hpipm
+from . import hpipm 
 from . import external 
-
-from .hpipm import *
-from .external import *
+from hpipm_python.external.blasfeo.wrapper import *
