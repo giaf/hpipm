2bb39a417d21a5a3407576928446cd392817ae0b
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-03 14:15:54 +0200

    fixed top-level availability of common structures - adding dummy example

---
+++ interfaces/python/hpipm_python/examples/getting_started.py
@@ -0,0 +1,3 @@
+from hpipm_python import *
+A = blasfeo_dmat()
+ocp_dim = d_ocp_qp_dim()
--- interfaces/python/hpipm_python/hpipm_python/__init__.py
+++ interfaces/python/hpipm_python/hpipm_python/__init__.py
@@ -4,8 +4,5 @@ This is the Python interface to the QP solver HPIPM.
 Copyright 2018 (c) Andrea Zanelli, Gianluca Frison 
 """
 
-from . import hpipm 
-from . import external 
-from hpipm_python.external.blasfeo.wrapper import *
-from hpipm_python.hpipm.d_ocp_qp.wrapper import *
-from hpipm_python.hpipm.core.wrapper import *
+from .external.blasfeo.wrapper import *
+from .hpipm.d_ocp_qp.wrapper import *
