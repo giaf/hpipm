bead4c414b7ebca68fe36632e5209c09c420c81a
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-03 17:09:10 +0200

    import libhpipm.so

--- interfaces/python/hpipm_python/hpipm_python/wrapper/hpipm_solver.py
+++ interfaces/python/hpipm_python/hpipm_python/wrapper/hpipm_solver.py
@@ -2,11 +2,17 @@ from hpipm_python.external.blasfeo.wrapper import *
 from hpipm_python.hpipm.core.wrapper import *
 from hpipm_python.hpipm.d_ocp_qp.wrapper import *
 
+from ctypes import *
+
 import numpy as np
 
 class hpipm_solver:
-    def __init__(self, d_ocp_qp_dim: dim):
-
+    def __init__(self, dim, N):
+        
+        _h = CDLL('libhpipm.so')
+        dim_size = _h.d_memsize_ocp_qp_dim(N)
+        dim_mem = c_void_p()
+        v_zeros(byref(dim_size), dim_mem)
 
 
 
