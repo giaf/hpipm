256cdca906952e4cc74a3cad8d5d21b2fbf0945a
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-03 15:49:17 +0200

    adding hpipm_solver top class

---
+++ interfaces/python/hpipm_python/hpipm_python/wrapper/hpipm_solver.py
@@ -0,0 +1,12 @@
+from hpipm_python.external.blasfeo.wrapper import *
+from hpipm_python.hpipm.core.wrapper import *
+from hpipm_python.hpipm.d_ocp_qp.wrapper import *
+
+import numpy as np
+
+class hpipm_solver:
+    def __init__(self, d_ocp_qp_dim: dim):
+
+
+
+
