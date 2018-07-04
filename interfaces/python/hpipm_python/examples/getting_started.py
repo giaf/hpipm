6e0e1299a43256600d6bd6a36cd8684c55012499
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-04 22:11:53 +0200

    fixed example

--- interfaces/python/hpipm_python/examples/getting_started.py
+++ interfaces/python/hpipm_python/examples/getting_started.py
@@ -18,18 +18,18 @@ qp_data.B = [B, B, B, B, B]
 qp_data.b = [b, b, b, b, b]
 qp_data.Q = [Q, Q, Q, Q, Q, Q]
 qp_data.S = [S, S, S, S, S, S]
-qp_data.R = [R, R, R, R, R]
+qp_data.R = [R, R, R, R, R, R]
 qp_data.q = [q, q, q, q, q, q]
 qp_data.r = [r, r, r, r, r, r]
 
 x0 = np.array([1, 1])
 
-qp_data.lb = [x0]
-qp_data.ub = [x0]
+qp_data.d_lb = [x0]
+qp_data.d_ub = [x0]
+
+qp_data.idxb = [np.array([1, 2])]
 
 qp_dims = hpipm_dims()
-
-qp_dims.idxb0 = np.array([1, 2])
 
 qp_dims.nx = np.array([2, 2, 2, 2, 2, 2])
 qp_dims.nu = np.array([1, 1, 1, 1, 1, 0])
@@ -38,7 +38,12 @@ qp_dims.ng = np.array([0, 0, 0, 0, 0, 0])
 qp_dims.ns = np.array([0, 0, 0, 0, 0, 0])
 qp_dims.nbx = np.array([2, 0, 0, 0, 0, 0])
 qp_dims.nbu = np.array([0, 0, 0, 0, 0, 0])
-qp_dims.N = 10
+qp_dims.N = 5
 
+# set up solver
 solver = hpipm_solver(qp_dims, qp_data)
 
+# solve qp
+return_flag = solver.solve()
+
+print('hpipm returned with flag {0:2d}'.format(return_flag))
