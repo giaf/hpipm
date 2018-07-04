f84db58d9b1e510c30af31f617194d02e41f1a4e
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-04 09:32:56 +0200

    fixed segfault

--- interfaces/python/hpipm_python/examples/getting_started.py
+++ interfaces/python/hpipm_python/examples/getting_started.py
@@ -1,3 +1,15 @@
 from hpipm_python import *
-A = blasfeo_dmat()
-ocp_dim = d_ocp_qp_dim()
+import numpy as np
+
+N = 10
+
+nx = np.array([2, 2, 2, 2, 2, 2])
+nu = np.array([1, 1, 1, 1, 1, 0])
+nb = np.array([2, 0, 0, 0, 0, 0])
+ng = np.array([0, 0, 0, 0, 0, 0])
+ns = np.array([0, 0, 0, 0, 0, 0])
+nbx = np.array([2, 0, 0, 0, 0, 0])
+nbu = np.array([0, 0, 0, 0, 0, 0])
+
+solver = hpipm_solver(N, nx, nu, nbx, nbu, ng, ns)
+
