7976f2e5078db0d5113e9556c1d4034a141dd710
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-05 14:29:34 +0200

    retabbing

--- interfaces/python/hpipm_python/hpipm_python/external/blasfeo/wrapper.py
+++ interfaces/python/hpipm_python/hpipm_python/external/blasfeo/wrapper.py
@@ -4,13 +4,13 @@ from os import *
 class blasfeo_dmat(Structure):
     _fields_ = [    
         ("m", c_int),                  # rows
-	("n", c_int),                  # cols
-	("pm", c_int),                 # packed number or rows
-	("cn", c_int),                 # packed number or cols
-	("pA", POINTER(c_double)),     # pointer to a pm*pn array of doubles, the first is aligned to cache line size
-	("dA", POINTER(c_double)),     # pointer to a min(m,n) (or max???) array of doubles
-	("use_dA", c_int),             # flag to tell if dA can be used
-	("memsize", c_int)             # size of needed memory
+        ("n", c_int),                  # cols
+        ("pm", c_int),                 # packed number or rows
+        ("cn", c_int),                 # packed number or cols
+        ("pA", POINTER(c_double)),     # pointer to a pm*pn array of doubles, the first is aligned to cache line size
+        ("dA", POINTER(c_double)),     # pointer to a min(m,n) (or max???) array of doubles
+        ("use_dA", c_int),             # flag to tell if dA can be used
+        ("memsize", c_int)             # size of needed memory
         ]
 
 
@@ -19,7 +19,7 @@ class blasfeo_dmat(Structure):
 class blasfeo_dvec(Structure):
     _fields_ = [    
         ("m", c_int),                   # size
-	("pm", c_int),                  # packed size
+        ("pm", c_int),                  # packed size
         ("pa", POINTER(c_double)),      # pointer to a pm array of doubles, the first is aligned to cache line size
-	("memsize", c_int)              # size of needed memory
+        ("memsize", c_int)              # size of needed memory
         ]
