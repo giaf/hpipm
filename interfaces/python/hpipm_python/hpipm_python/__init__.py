efeaf539d598aace0cfd99ed53a490643375d548
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-23 16:31:31 +0200

    adding common imports

--- interfaces/python/hpipm_python/hpipm_python/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-This is the Python interface to the QP solver HPIPM.
-Copyright 2018 (c) Andrea Zanelli, Gianluca Frison 
-"""
-
-from .external.blasfeo.wrapper import *
-from .hpipm.d_ocp_qp.wrapper import *
-from .wrapper.hpipm_solver import *
