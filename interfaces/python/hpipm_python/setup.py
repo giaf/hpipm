393e2b909b3616e98b09376c1b2835be7fe97b28
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-23 16:55:17 +0200

    fix discovery of subpackages

--- interfaces/python/hpipm_python/setup.py
+++ interfaces/python/hpipm_python/setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from pkgutil import walk_packages
 
-setup(name='hpipm_python',
-      version='0.1',
-      description='Python interface to HPIPM',
-      url='http://github.com/giaf/hpipm',
-      author='Andrea Zanelli - Gianluca Frison',
-      license='LGPL',
-      packages=['hpipm_python'],
-      zip_safe=False)
+import hpipm_python
+
+# def find_packages(path=__path__, prefix=""):
+#     yield prefix
+#     prefix = prefix + "."
+#     for _, name, ispkg in walk_packages(path, prefix):
+#         if ispkg:
+#             yield name
+
+setup(name='hpipm-python',
+   version='0.1',
+   description='Python interface to HPIPM',
+   url='http://github.com/giaf/hpipm',
+   author='Andrea Zanelli - Gianluca Frison',
+   license='LGPL',
+   packages = find_packages(),
+   zip_safe=False)
