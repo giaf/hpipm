c5fe9526fb9f0ea9e143c47e728fe5d2c1ee55f2
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-07-30 20:23:35 +0200

    Revert "adding print level field to args, retabbing"
    This reverts commit 70c0ced68e356ba2581442b989d6a856e6ea222c.

--- include/hpipm_s_dense_qp_ipm.h
+++ include/hpipm_s_dense_qp_ipm.h
@@ -50,71 +50,70 @@ extern "C" {
 
 
 struct s_dense_qp_ipm_arg
-    {
-    float mu0; // initial value for duality measure
-    float alpha_min; // exit cond on step length
-    float res_g_max; // exit cond on inf norm of residuals
-    float res_b_max; // exit cond on inf norm of residuals
-    float res_d_max; // exit cond on inf norm of residuals
-    float res_m_max; // exit cond on inf norm of residuals
-    float reg_prim; // reg of primal hessian
-    float reg_dual; // reg of dual hessian
-    float lam_min; // min value in lam vector
-    float t_min; // min value in t vector
-    int iter_max; // exit cond in iter number
-    int stat_max; // iterations saved in stat
-    int pred_corr; // Mehrotra's predictor-corrector IPM algirthm
-    int cond_pred_corr; // conditional Mehrotra's predictor-corrector
-    int scale; // scale hessian
-    int itref_pred_max; // max number of iterative refinement steps for predictor step
-    int itref_corr_max; // max number of iterative refinement steps for corrector step
-    int warm_start; // 0 no warm start, 1 warm start primal sol
-    int lq_fact; // 0 syrk+potrf, 1 mix, 2 lq
-    int abs_form; // absolute IPM formulation
-    int comp_res_exit; // compute residuals on exit (only for abs_form==1)
-    int memsize;
-    int print_level;
-    };
+	{
+	float mu0; // initial value for duality measure
+	float alpha_min; // exit cond on step length
+	float res_g_max; // exit cond on inf norm of residuals
+	float res_b_max; // exit cond on inf norm of residuals
+	float res_d_max; // exit cond on inf norm of residuals
+	float res_m_max; // exit cond on inf norm of residuals
+	float reg_prim; // reg of primal hessian
+	float reg_dual; // reg of dual hessian
+	float lam_min; // min value in lam vector
+	float t_min; // min value in t vector
+	int iter_max; // exit cond in iter number
+	int stat_max; // iterations saved in stat
+	int pred_corr; // Mehrotra's predictor-corrector IPM algirthm
+	int cond_pred_corr; // conditional Mehrotra's predictor-corrector
+	int scale; // scale hessian
+	int itref_pred_max; // max number of iterative refinement steps for predictor step
+	int itref_corr_max; // max number of iterative refinement steps for corrector step
+	int warm_start; // 0 no warm start, 1 warm start primal sol
+	int lq_fact; // 0 syrk+potrf, 1 mix, 2 lq
+	int abs_form; // absolute IPM formulation
+	int comp_res_exit; // compute residuals on exit (only for abs_form==1)
+	int memsize;
+	};
 
 
 
 struct s_dense_qp_ipm_workspace
-    {
-    struct s_core_qp_ipm_workspace *core_workspace;
-    struct s_dense_qp_res_workspace *res_workspace;
-    struct s_dense_qp_sol *sol_step;
-    struct s_dense_qp_sol *sol_itref;
-    struct s_dense_qp *qp_step;
-    struct s_dense_qp *qp_itref;
-    struct s_dense_qp_res *res;
-    struct s_dense_qp_res *res_itref;
-    struct blasfeo_svec *Gamma; //
-    struct blasfeo_svec *gamma; //
-    struct blasfeo_svec *Zs_inv; //
-    struct blasfeo_smat *Lv; //
-    struct blasfeo_smat *AL; //
-    struct blasfeo_smat *Le; //
-    struct blasfeo_smat *Ctx; //
-    struct blasfeo_svec *lv; //
-    struct blasfeo_svec *sv; // scale for Lv
-    struct blasfeo_svec *se; // scale for Le
-    struct blasfeo_svec *tmp_nbg; // work space of size nb+ng
-    struct blasfeo_svec *tmp_ns; // work space of size ns
-    struct blasfeo_smat *lq0;
-    struct blasfeo_smat *lq1;
-    struct blasfeo_svec *tmp_m;
-    float *stat; // convergence statistics
-//  int *ipiv_v;
-//  int *ipiv_e;
-    void *lq_work0;
-    void *lq_work1;
-    float qp_res[4]; // infinity norm of residuals
-    int iter; // iteration number
-    int stat_max; // iterations saved in stat
-    int scale;
-    int use_hess_fact;
-    int memsize; // memory size (in bytes) of workspace
-    };
+	{
+	struct s_core_qp_ipm_workspace *core_workspace;
+	struct s_dense_qp_res_workspace *res_workspace;
+	struct s_dense_qp_sol *sol_step;
+	struct s_dense_qp_sol *sol_itref;
+	struct s_dense_qp *qp_step;
+	struct s_dense_qp *qp_itref;
+	struct s_dense_qp_res *res;
+	struct s_dense_qp_res *res_itref;
+	struct blasfeo_svec *Gamma; //
+	struct blasfeo_svec *gamma; //
+	struct blasfeo_svec *Zs_inv; //
+	struct blasfeo_smat *Lv; //
+	struct blasfeo_smat *AL; //
+	struct blasfeo_smat *Le; //
+	struct blasfeo_smat *Ctx; //
+	struct blasfeo_svec *lv; //
+	struct blasfeo_svec *sv; // scale for Lv
+	struct blasfeo_svec *se; // scale for Le
+	struct blasfeo_svec *tmp_nbg; // work space of size nb+ng
+	struct blasfeo_svec *tmp_ns; // work space of size ns
+	struct blasfeo_smat *lq0;
+	struct blasfeo_smat *lq1;
+	struct blasfeo_svec *tmp_m;
+	float *stat; // convergence statistics
+//	int *ipiv_v;
+//	int *ipiv_e;
+	void *lq_work0;
+	void *lq_work1;
+	float qp_res[4]; // infinity norm of residuals
+	int iter; // iteration number
+	int stat_max; // iterations saved in stat
+	int scale;
+	int use_hess_fact;
+	int memsize; // memory size (in bytes) of workspace
+	};
 
 
 
