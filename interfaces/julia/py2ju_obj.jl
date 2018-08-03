8d320185eac47159b8fcf9f0ef9c42b350fad111
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-08-03 19:03:31 +0200

    skipping base class attributes

--- interfaces/julia/py2ju_obj.jl
+++ interfaces/julia/py2ju_obj.jl
@@ -3,10 +3,14 @@ function py2ju_obj(py_obj)
     n_prop = length(prop)
     ju_obj = Dict()
     for i = 1:n_prop
-        if py_obj[prop[i]] != nothing
-            error("Cannot convert Python object with non-None attribute values.")
+        # skip base class attributes
+        if !contains(String(prop[i]), "__") 
+            if py_obj[prop[i]] != nothing
+                error("Cannot convert Python object with non-None attribute value $(prop[i]).")
+            end
+            entry_name = String(prop[i])
+            ju_obj[entry_name] = nothing
         end
-        entry_name = String(prop[i])
-        ju_obj[entry_name] = nothing
     end
+    return ju_obj
 end
