ea1f0a7a2d256d82e0583bddbdec0a04e8a406db
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-08-04 16:57:05 +0200

    adding some comments

--- interfaces/julia/ju2py_obj.jl
+++ interfaces/julia/ju2py_obj.jl
@@ -11,22 +11,26 @@ function ju2py_obj(ju_obj, py_obj)
             ju_key_name = String(py_attr_name)
             if ju_obj[ju_key_name] != nothing
                 ju_eltype = eltype(ju_obj[ju_key_name])
+                # assign Julia array of arrays to Python list of numpy arrays
                 if ju_eltype == Array{Int64, 1} || ju_eltype == Array{Float64,1} || ju_eltype == Array{Int64,2} || ju_eltype == Array{Float64, 2}
                     try py_obj[py_attr_name] = PyVector(ju_obj[ju_key_name])
                     catch
                         error("Could not assign attribute $(py_attr[i]).")
                     end
+                # assign Julia array to numpy array
                 elseif (ju_eltype == Int64 || ju_eltype == Float64) && size(ju_obj[ju_key_name]) != ()
                     try py_obj[py_attr_name] = np.array(ju_obj[ju_key_name])
                     catch
                         error("Could not assign attribute $(py_attr[i]).")
                     end
+                # assign scalar
                 elseif size(ju_obj[ju_key_name]) == ()
                     try py_obj[py_attr_name] = ju_obj[ju_key_name]
                     catch
                         error("Could not assign attribute $(py_attr[i]).")
                     end
                 else
+                # the Julia Dictionary containts something that it should not contain
                     error("Julia dictionary contains something not an Array{Int/Float64} or Int/Float64.")
                 end
             end
