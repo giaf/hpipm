9cb02eb8e758b28973991f56ba8b58383a937442
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-08-04 12:04:32 +0200

    adding hpipm_solver julia module

---
+++ interfaces/julia/hpipm_solver.jl
@@ -0,0 +1,11 @@
+function hpipm_data()
+    py_qp_data = hp.hpipm_dims()
+    ju_qp_data = py2ju_obj(py_qp_data)
+    return ju_qp_data
+end
+
+function hpipm_dims()
+    py_qp_dims = hp.hpipm_dims()
+    ju_qp_dims = py2ju_obj(py_qp_dims)
+    return ju_qp_dims
+end
