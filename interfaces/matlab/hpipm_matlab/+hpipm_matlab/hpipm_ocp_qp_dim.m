e6c92f9f45e31a74b20f94714954321e36a3958c
zanellia <andrea.zanelli@imtek.uni-freiburg.de>
2018-10-07 12:29:11 +0200

    added unique setter for dim (MATLAB/Octave)

--- interfaces/matlab/hpipm_matlab/+hpipm_matlab/hpipm_ocp_qp_dim.m
+++ interfaces/matlab/hpipm_matlab/+hpipm_matlab/hpipm_ocp_qp_dim.m
@@ -21,6 +21,26 @@ classdef hpipm_ocp_qp_dim
 		end
 
 
+		function set(obj, varargin)
+			if(length(varargin)==2)
+				field = varargin{1};
+				n_dim = varargin{2};
+				m = length(n_dim);
+				py_n_dim = obj.np.zeros({int32(m), int32(1)}, obj.np.int32);
+				for i=1:m
+					py_n_dim.itemset(int32(i-1), int32(0), int32(n_dim(i)));
+				end
+				obj.py_dims.set(field, py_n_dim);
+			else
+				field = varargin{1};
+				n_dim = varargin{2};
+				idx = varargin{3};
+				obj.py_dims.set(field, int32(n_dim), int32(idx));
+			end
+		end
+
+
+
 		function set_nx(obj, varargin)
 			if(length(varargin)==1)
 				nx = varargin{1};
