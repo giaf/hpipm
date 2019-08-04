3765394cf1300c406c2efdc336c0890aee0ca3dd
giaf <gianfri2008@hotmail.it>
2019-08-04 17:08:22 +0200

    change license to BSD-2

--- utils/change_lic.sh
+++ utils/change_lic.sh
@@ -1,6 +1,23 @@
 #! /bin/bash
 
-#find . -name "*.S" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +29 $file >> temp && cat temp > $file; done;' {} +
-#find . -name "*.c" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +29 $file >> temp && cat temp > $file; done;' {} +
-#find . -name "*.h" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +29 $file >> temp && cat temp > $file; done;' {} +
-#find . -name "Makefile.*" -exec bash -c 'for file; do cat utils/new_lic.makefile >temp && tail -n +29 $file >> temp && cat temp > $file; done;' {} +
+#for file in `find test_problems/ -name "*.h"`; do echo $file; done
+
+#for file in `find auxiliary/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find cond/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find dense_qp/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find ipm_core/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find ocp_qp/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find tree_ocp_qp/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find include/ -name "*.h"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find benchmark/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find benchmark/ -name "*.h"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find test_problems/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find test_problems/ -name "*.h"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find . -name "Makefile*"`; do cat utils/new_lic_makefile > temp && tail -n +35 $file >> temp && cat temp > $file; done
+
+#for file in `find interfaces/matlab_octave/ -name "*.c"`; do cat utils/new_lic_c > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find interfaces/matlab_octave/ -name "*.m"`; do cat utils/new_lic_matlab > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find examples/matlab_octave/ -name "*.m"`; do cat utils/new_lic_matlab > temp && tail -n +35 $file >> temp && cat temp > $file; done
+
+#for file in `find interfaces/python/ -name "*.py"`; do cat utils/new_lic_python > temp && tail -n +35 $file >> temp && cat temp > $file; done
+#for file in `find examples/python/ -name "*.py"`; do cat utils/new_lic_python > temp && tail -n +35 $file >> temp && cat temp > $file; done
