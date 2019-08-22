1953ad05e74baba95ecfd6f79ae3cdcdf65a7d69
giaf <gianfri2008@hotmail.it>
2019-08-22 17:25:44 +0200

    small fix

--- dense_qp/s_dense_qp.c
+++ dense_qp/s_dense_qp.c
@@ -107,8 +107,8 @@
 #define DENSE_QP_GET_ZU s_dense_qp_get_zu
 #define DENSE_QP_GET_LS s_dense_qp_get_ls
 #define DENSE_QP_GET_US s_dense_qp_get_us
-#define DENSE_QP_SET_ALL_ROWMAJ d_dense_qp_set_all_rowmaj
-#define DENSE_QP_GET_ALL_ROWMAJ d_dense_qp_get_all_rowmaj
+#define DENSE_QP_SET_ALL_ROWMAJ s_dense_qp_set_all_rowmaj
+#define DENSE_QP_GET_ALL_ROWMAJ s_dense_qp_get_all_rowmaj
 
 #include "x_dense_qp.c"
 
