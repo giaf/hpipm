4dc08f7c0c729c01967031e8ee84e70f93932dc4
sandmaennchen <katrin.baumgaertner@posteo.de>
2024-01-12 14:24:24 +0100

    consistent return types (return a list whenever idx_end is given)

--- interfaces/python/hpipm_python/hpipm_python/wrapper/hpipm_ocp_qp_sol.py
+++ interfaces/python/hpipm_python/hpipm_python/wrapper/hpipm_ocp_qp_sol.py
@@ -36,7 +36,7 @@
 from ctypes import *
 import numpy as np
 from .hpipm_ocp_qp_dim import hpipm_ocp_qp_dim
-
+from typing import Union, Optional
 
 class hpipm_ocp_qp_sol:
 
@@ -103,21 +103,29 @@ class hpipm_ocp_qp_sol:
 			}
 		}
 
-	def get(self, field, idx_start, idx_end=None):
+	def get(self, field: str, idx_start: int, idx_end: Optional[int] = None) -> Union[np.ndarray, list[np.ndarray]]:
+		'''
+		Getter for value of `field` at stage `idx_start` or at stages `idx_start` to `idx_end`.
+		Returns a single np.ndarray if only `idx_start` is given.
+		Returns a list of np.ndarray if also `idx_end` is given.
+		'''
+
 		if field not in self.__getters:
-			raise NameError('hpipm_ocp_qp_sol.get: wrong field')
+			raise NameError('hpipm_ocp_qp_sol.get: Wrong field. Available fields are:', *self.__getters.keys())
 		else:
 			return self.__get(self.__getters[field], idx_start, idx_end)
 
-	def __get(self, getter, idx_start, idx_end=None):
-		# number of variables
+
+	def __get(self, getter, idx_start: int, idx_end: Optional[int]=None) -> Union[np.ndarray, list[np.ndarray]]:
 		n_var = np.zeros((1,1), dtype=int)
 
 		if idx_end is None:
-			idx_end = idx_start
+			idx_end_ = idx_start
+		else:
+			idx_end_ = idx_end
 
 		var = []
-		for i in range(idx_start, idx_end + 1):
+		for i in range(idx_start, idx_end_ + 1):
 			# get number of variables at stage
 			tmp_ptr = cast(n_var.ctypes.data, POINTER(c_int))
 			getter['n_var'](self.dim.dim_struct, i, tmp_ptr)
@@ -126,7 +134,8 @@ class hpipm_ocp_qp_sol:
 			tmp_ptr = cast(var[-1].ctypes.data, POINTER(c_double))
 			getter['var'](i, self.qp_sol_struct, tmp_ptr)
 
-		return var if len(var) > 1 else var[0]
+		return var if idx_end is not None else var[0]
+
 
 	def print_C_struct(self):
 		self.__hpipm.d_ocp_qp_sol_print(self.dim.dim_struct, self.qp_sol_struct)
